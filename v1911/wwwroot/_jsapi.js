3c4edea89f3f33283577b1923471559f327b4253
xiechaoyi <mail@xiechaoyi.com>
2020-04-11 13:01:06 +0800

    Signed-off-by: xiechaoyi <mail@xiechaoyi.com>

--- v1911/wwwroot/_jsapi.js
+++ v1911/wwwroot/_jsapi.js
@@ -1,17 +1,25 @@
-﻿var xchost = '';
-function xcOpenTab(url, name) {
+﻿var ehost = '';
+function emiOpenTab(url, name) {
 	let ifr = document.createElement('iframe');
-	ifr.id = 'xc_ifr';
-	ifr.name = 'xc_ifr';
-	ifr.src = '//' + xchost + '/jsapi?do=open&url=' + encodeURI(url) + '&name=' + encodeURI(name);
+	ifr.id = 'emifr';
+	ifr.name = 'emifr';
+	ifr.src = '//' + ehost + '/jsapi?do=open&url=' + encodeURI(url) + '&name=' + encodeURI(name);
 	ifr.style.display = 'none';
 	document.body.appendChild(ifr);
 }
-function xcBackTab(name) {
+function emiBackTab(name) {
 	let ifr = document.createElement('iframe');
-	ifr.id = 'xc_ifr';
-	ifr.name = 'xc_ifr';
-	ifr.src = '//' + xchost + '/jsapi?do=back&name=' + encodeURI(name);
+	ifr.id = 'emifr';
+	ifr.name = 'emifr';
+	ifr.src = '//' + ehost + '/jsapi?do=back&name=' + encodeURI(name);
+	ifr.style.display = 'none';
+	document.body.appendChild(ifr);
+}
+function emiToLogin() {
+	let ifr = document.createElement('iframe');
+	ifr.id = 'emifr';
+	ifr.name = 'emifr';
+	ifr.src = '//' + ehost + '/jsapi?do=tologin';
 	ifr.style.display = 'none';
 	document.body.appendChild(ifr);
 }
