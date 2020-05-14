1d8dd15d168132363914cc9c5b302a13489fcfd4
xiechaoyi <mail@xiechaoyi.com>
2020-05-14 22:24:26 +0800

    Signed-off-by: xiechaoyi <mail@xiechaoyi.com>

---
+++ v2005/wwwroot/_jsapi.js
@@ -0,0 +1,29 @@
+﻿var ehost = '';
+function emiOpenTab(url, name) {
+	if (!parent || parent === self) {
+		location.href = url
+	} else {
+		let ifr = document.createElement('iframe');
+		ifr.id = 'emifr';
+		ifr.name = 'emifr';
+		ifr.src = '//' + ehost + '/jsapi?do=open&url=' + encodeURIComponent(url) + '&name=' + encodeURI(name);
+		ifr.style.display = 'none';
+		document.body.appendChild(ifr);
+	}
+}
+function emiBackTab(name) {
+	let ifr = document.createElement('iframe');
+	ifr.id = 'emifr';
+	ifr.name = 'emifr';
+	ifr.src = '//' + ehost + '/jsapi?do=back&name=' + encodeURI(name);
+	ifr.style.display = 'none';
+	document.body.appendChild(ifr);
+}
+function emiToLogin() {
+	let ifr = document.createElement('iframe');
+	ifr.id = 'emifr';
+	ifr.name = 'emifr';
+	ifr.src = '//' + ehost + '/jsapi?do=tologin';
+	ifr.style.display = 'none';
+	document.body.appendChild(ifr);
+}
