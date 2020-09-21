d0c5102a1b7d0dfe03bba7ab08434468740b1b5e
xcy <mail@xiechaoyi.com>
2020-09-21 15:06:14 +0800

    GitPush

--- GitPush.bat
+++ GitPush.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p tags=Input tag(latest):
+set /p tags=Input tag name to push(latest):
 if "%tags%"=="" (
     git add -A && git commit -a -m "GitPush" && git push -u origin master
 ) else (
