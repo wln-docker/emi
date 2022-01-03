991ef530a8fc2980bdef5ce1fe5910603549cee9
xcy <mail@xiechaoyi.com>
2022-01-03 23:28:48 +0800

    Signed-off-by: xcy <mail@xiechaoyi.com>

--- Docker Publish.bat
+++ Docker Publish.bat
@@ -1,4 +1,3 @@
-docker build -t wlniao/emi:latest v2105
-docker build -t wlniao/emi:v2105 v2105
-docker push wlniao/emi:latest
-docker push wlniao/emi:v2105
+docker build -t wlniao/emi:v2111 v2111
+docker build -t wlniao/emi v2111
+docker push wlniao/emi
